388bdf092b6ac702192c4bf62640c55e9adfd6b6
kristinardis <kamcllvenna@nic.edu>
2023-10-01 16:30:31 -0700

    assignments completed

--- javascript.js
+++ javascript.js
@@ -1,13 +1,66 @@
 // 1.1 Assignment: Where do you put JavaScript?
+alert("Javascript is Easy");
 
 // 1.2 Assignment: Variables
+var name = "John";
+var age = 25;
+var nationality = "United Kingdom";
+var gender = "Male";
 
+console.log(name);
+console.log(age);
+console.log(nationality);
+console.log(gender);
 // 1.3 Assignment: Variable Declaration & Initialization
+var a = "Christmas";
+var b = "Halloween";
+var c = "Birthday";
+var d = "Mother's Day";
+var e;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
 
 // 1.4 Assignment: Complete your statements
+x = 2; y = 3;
+
+z = x + y;
+
+console.log(x);
+console.log(y);
+console.log(z);
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
+var a = function(){
+    alert('Statement 1');
+    alert('Statement 2');
+}
+// i1.6 Assignment: Data Ty
+//string
+var a="Kris McILvenna";
 
-// 1.6 Assignment: Data Types
+//number
+var b = 39;
+
+//boolean
+var c = true;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+
 
 // 1.7 Assignment: Practicing JavaScript basics
+var a = 4;
+var b = 3;
+var c = 5;
+var area = b*a/2;
+
+console.log(area);
